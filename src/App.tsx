d807460df0f6aec25997bc2581f1bb38bb7ee421
Victor Queiroz <victoorqrz@gmail.com>
2021-02-22 19:19:06 -0300

    ExperienceBar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 

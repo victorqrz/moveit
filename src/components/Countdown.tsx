cfae527222bcfc28388a4501633452255f26d6dd
Victor Queiroz <victoorqrz@gmail.com>
2021-02-25 16:32:50 -0300

    adding the checked icon to the disabled button

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -56,6 +56,7 @@ export function Countdown() {
           className={styles.countdownButton}
         >
           Ciclo encerrado
+          <img src="icons/checked.svg" alt=""/>
         </button>
       ) : (
         <>

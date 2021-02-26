40e1d8580626ca884c0f90b95c824842e6f4b7d5
Victor Queiroz <victoorqrz@gmail.com>
2021-02-26 19:33:42 -0300

    active ChallengeBox done

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -7,7 +7,30 @@ export function ChallengeBox() {
   return (
     <div className={styles.challengeBoxContainer}>
       { hasActiveChallenge ? (
-        <div></div>
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" alt="New Challenge"/>
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos</p>
+          </main>
+
+          <footer>
+            <button 
+              type="button"
+              className={styles.challengeFailedButton}
+            >
+              Falhei
+            </button>
+            <button 
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
       ) : (
         <div className={styles.challengeNotActive}>
           <strong>Finalize um ciclo para receber um desafio</strong>
